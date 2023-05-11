a082598266c205d8bbd67e508060c214b8e0bf11
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-05-11 19:30:09 -0300

    select/unselect action

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
 console.log("olá!");
+
+document
+  .querySelector("#rating-btn1")
+  .addEventListener("click", changeColorOrange1);
+
+function changeColorOrange1() {
+  document.querySelector("#rating-btn1").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn2").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn3").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn4").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn5").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn1").style.backgroundColor = "#fb7413";
+}
+
+document
+  .querySelector("#rating-btn2")
+  .addEventListener("click", changeColorOrange2);
+
+function changeColorOrange2() {
+  document.querySelector("#rating-btn1").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn2").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn3").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn4").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn5").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn2").style.backgroundColor = "#fb7413";
+}
+
+document
+  .querySelector("#rating-btn3")
+  .addEventListener("click", changeColorOrange3);
+
+function changeColorOrange3() {
+  document.querySelector("#rating-btn1").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn2").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn3").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn4").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn5").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn3").style.backgroundColor = "#fb7413";
+}
+
+document
+  .querySelector("#rating-btn4")
+  .addEventListener("click", changeColorOrange4);
+
+function changeColorOrange4() {
+  document.querySelector("#rating-btn1").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn2").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn3").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn4").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn5").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn4").style.backgroundColor = "#fb7413";
+}
+
+document
+  .querySelector("#rating-btn5")
+  .addEventListener("click", changeColorOrange5);
+
+function changeColorOrange5() {
+  document.querySelector("#rating-btn1").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn2").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn3").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn4").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn5").style.backgroundColor = "blue";
+  document.querySelector("#rating-btn5").style.backgroundColor = "#fb7413";
+}
+
+document
+  .querySelector("#submit-btn")
+  .addEventListener("click", changeColorWhite);
+
+function changeColorWhite() {
+  document.querySelector("#submit-btn").style.backgroundColor = "#ffffff";
+  document.querySelector("#submit-btn").style.color = "#fb7413";
+}
